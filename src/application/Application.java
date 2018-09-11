125e3c8ea4fa2afe99e5f1f20708db7d39322e08
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-09-11 12:42:59 -0700

    updated connection file

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,8 +1,12 @@
 package application;
 
+import dao.DBConnection;
+
 public class Application {
 
 	public static void main(String[] args) {
+		
+		
 		
 		Menu menu = new Menu();
 		menu.start();
--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -4,32 +4,27 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+//not using strict singleton pattern
 public class DBConnection {
 	
 	private final static String URL = "jdbc:mysql://localhost:3306/books";
 	private final static String USERNAME = "user1";
 	private final static String PASSWORD = "password1";
 	private static Connection connection;
-	private static DBConnection instance; //singleton pattern
 	
-	private DBConnection(Connection connection) {
-		//cannot construct an instance of this class from outside this class: using a singleton
-		this.connection = connection;
-	}
 	
 	public static Connection getConnection() {
-		if (instance == null) {
+		if (connection == null) {
 			//if its null, it creates the connection
 			try {
 				connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-				instance = new DBConnection(connection); //call constructor
 				System.out.println("Connected to DB successfully!");
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		//if it is not null, it returns the already created connection
-		return DBConnection.connection;
+		return connection;
 	}
 
 }
